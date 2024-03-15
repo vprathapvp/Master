feb78b05567d6377f2c6e0c286ba464bab3e14b3
Dhivakar2411 <vdhivakar24@gmail.com>
2024-03-15 14:02:33 -0700

    Second Commit

---
+++ ScrollImage/app/src/main/java/com/dhivakar/scrollimage/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.dhivakar.scrollimage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize VideoViews
+        VideoView videoView1 = view.findViewById(R.id.v1);
+
+        // Set video paths
+        videoView1.setVideoPath("/storage/emulated/0/Movies/Dhivakar/video1.mp4");
+
+
+        // Initialize separate MediaControllers
+        MediaController mediaController1 = new MediaController(requireContext());
+
+
+        // Attach separate MediaControllers to VideoViews
+        videoView1.setMediaController(mediaController1);
+
+
+        // Set anchor views to avoid overlapping controls
+        mediaController1.setAnchorView(videoView1);
+
+        // Start playing videos
+
+
+
+        return view;
+    }
+}
---
+++ ScrollImage/app/src/main/java/com/dhivakar/scrollimage/MainActivity.java
@@ -0,0 +1,71 @@
+package com.dhivakar.scrollimage;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.shorts) {
+                selectedFragment = new ShortsFragment();
+            } else if (item.getItemId() == R.id.add) {
+                selectedFragment = new AddFragment();
+            } else if (item.getItemId() == R.id.subscription) {
+                selectedFragment = new SubscriptionFragment();
+            } else if (item.getItemId() == R.id.profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+                return true;
+            }
+
+            return false;
+        });  // <-- Closing parenthesis for setOnNavigationItemSelectedListener
+
+        // Set the default fragment when the activity is created
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.settings) ;
+        Toast.makeText(this, "Settings menu", Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+}
