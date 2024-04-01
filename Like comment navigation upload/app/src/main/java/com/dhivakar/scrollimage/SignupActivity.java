175a55d6d308f70a146f6c1e6521fe5e463726f5
Sathish288 <sathish22042002@gmail.com>
2024-04-01 11:18:45 +0530

    register and login in android

---
+++ Like comment navigation upload/app/src/main/java/com/dhivakar/scrollimage/SignupActivity.java
@@ -0,0 +1,140 @@
+package com.dhivakar.scrollimage;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SignupActivity extends AppCompatActivity {
+
+    EditText etFirstName, etLastName, etEmail, etPassword, etConfirmPassword;
+    Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        etFirstName = findViewById(R.id.firstname);
+        etLastName = findViewById(R.id.lastname);
+        etEmail = findViewById(R.id.emailid);
+        etPassword = findViewById(R.id.regpassword);
+        etConfirmPassword = findViewById(R.id.regconpassword);
+        btnRegister = findViewById(R.id.newregisterbtn);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        String firstName = etFirstName.getText().toString().trim();
+        String lastName = etLastName.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(SignupActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(SignupActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(SignupActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!isValidPassword(password)) {
+            Toast.makeText(SignupActivity.this, "Password must contain at least one letter, one special symbol, and one number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("firstName", firstName);
+            jsonObject.put("lastName", lastName);
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        new SignUpTask().execute(jsonObject.toString());
+    }
+    private boolean isValidPassword(String password) {
+        // Password must contain at least one letter, one special symbol, and one number
+        String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        return password.matches(passwordPattern);
+    }
+    private class SignUpTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL("http://192.168.2.62:8093/register");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setChunkedStreamingMode(0);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                out.write(params[0].getBytes());
+                out.flush();
+
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                return response.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            if (result != null) {
+                // Show toast message
+                Toast.makeText(SignupActivity.this, result, Toast.LENGTH_SHORT).show();
+
+                // Start LoginActivity
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finish SignUpActivity to prevent going back to it when pressing back button from LoginActivity
+            }
+            else {
+                // Show toast message for failure
+                Toast.makeText(SignupActivity.this, "Email already exists ", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+}
