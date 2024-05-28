0e3edaf1c5be7c4d5a464eb342c763dc7f02da46
Dhivakar2411 <vdhivakar24@gmail.com>
2024-05-28 00:59:42 -0800

    ui update

---
+++ Bytes/app/src/main/java/com/ngltech/bytes/Config.java
@@ -0,0 +1,8 @@
+package com.ngltech.bytes;
+
+public class Config {
+    // Base URL for the API endpoint
+    public static final String BASE_URL = "http://192.168.55.71:8090";
+
+    // You can add more configuration constants here, such as API keys, timeout values, etc.
+}
