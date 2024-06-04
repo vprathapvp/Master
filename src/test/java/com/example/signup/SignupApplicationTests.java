9e9a4351d15778820c9e8a6fe2e8f03dc0cb7706
pavithra417 <pavithramoorthi719@gmail.com>
2024-06-04 10:47:34 +0530

    added video filter

---
+++ src/test/java/com/example/signup/SignupApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.signup;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bytes.BytesApplication;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes = BytesApplication.class)
+class SpringBootUploadFilesDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
